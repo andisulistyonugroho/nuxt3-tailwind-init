3d88028123076304df055f92693b5c759b16ff02
andisulistyonugroho <andisulistyonugroho@gmail.com>
2022-11-10 22:05:45 +0700

    using inter font true A

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
     extend: {
       colors: {
         primary: defaultTheme.colors.green
+      },
+      fontFamily: {
+        sans: [
+          '"Inter var", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+          { fontFeatureSettings: '"cv02","cv03","cv04","cv11"' }
+        ]
       }
     }
   }
